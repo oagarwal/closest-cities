154479556134dcf24b854cbb3e5cde46e0679cfd
Oshin Agarwal <oagarwal@seas.upenn.edu>
2018-01-18 15:15:09 -0500

    Search

--- geodata.py
+++ geodata.py
@@ -1,14 +1,46 @@
 import pandas as pd
+from haversine import haversine
 
 class GeoData:
 
 	def __init__(self):
 		self.df = pd.read_csv('data/cities1000.txt',header=None,names=['geonameid','name','asciiname','alternatenames','latitude','longitude','feature_class','feature_code','country_code','cc2','admin1_code','admin2_code','admin3_code','admin4_code','population','elevation','dem','timezone','modification_date'],sep='\t',encoding='utf-8')
-		self.df.set_index(['latitude','longitide'])
+		self.df['name_lower'] = self.df.apply(lambda row: row['name'].lower(),axis=1)
+		self.df.set_index(['latitude','longitude'])
 
-	def get_city_by_id(geonameid):
+	def get_city_by_id(self,geonameid):
 		return self.df.loc[self.df['geonameid'] == geonameid]
 
-	def get_city_by_coordinates(latitude,longitude,offset):
+	def get_city_by_coordinates(self,latitude,longitude,offset):
         	return self.df.loc[(self.df['latitude'] < latitude+offset) & (self.df['latitude'] > latitude-offset) & (self.df['longitude'] < longitude+offset) & (self.df['longitude'] > longitude-offset)]
+
+	def get_city_by_coordinates_and_country(self,latitude,longitude,offset,country_code):
+		return self.df.loc[(self.df['latitude'] < latitude+offset) & (self.df['latitude'] > latitude-offset) & (self.df['longitude'] < longitude+offset) & (self.df['longitude'] > longitude-offset) & (self.df['country_code'] == country_code)]
+	
+	def get_city_by_name(self,name):
+		name=name.lower().replace(' ','|')
+		return self.df.loc[self.df['name_lower'].str.contains(name)][['geonameid','name','country_code','latitude','longitude']]
+
+	def get_k_closest_cities(self,geonameid,k,same_country):
+		given_city = self.get_city_by_id(geonameid)
+		if len(given_city) == 0:
+			return None
+		else:
+			latitude = given_city.iloc[0]['latitude']
+			longitude = given_city.iloc[0]['longitude']
+			country_code = given_city.iloc[0]['country_code']
+
+			closest_cities = []
+			for offset in xrange(1,6):
+				if same_country:
+					temp_df = self.get_city_by_coordinates_and_country(latitude,longitude,offset,country_code)
+				else:
+					temp_df = self.get_city_by_coordinates(latitude,longitude,offset)
+				if len(temp_df)>k:
+					break
+		
+			for index,row in temp_df.iterrows():
+				closest_cities.append((row[['geonameid','name','country_code','latitude','longitude']],haversine((latitude,longitude),(row['latitude'],row['longitude']),miles=True)))
+
+			return sorted(closest_cities,key=lambda x:x[1])[:min(k+1,len(closest_cities))]
 
